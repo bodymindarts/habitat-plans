b32599090816033669314741a6b582ff75a13062
Justin Carter <justin@starkandwayne.com>
2017-05-07 09:39:26 +0200

    Build ocaml with world.opt

--- ocaml/plan.sh
+++ ocaml/plan.sh
@@ -7,13 +7,13 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL')
 pkg_source="https://github.com/ocaml/ocaml/archive/${pkg_version}.tar.gz"
 pkg_shasum="03e49d09d5a509216ca0cc8fccd10df3ad9dd441d7633e89974a74e149be3c51"
-pkg_deps=(core/glibc)
+pkg_deps=(core/glibc core/libev core/gmp core/zlib)
 pkg_build_deps=(core/make core/gcc)
 pkg_lib_dirs=(lib)
 pkg_bin_dirs=(bin)
 
 do_build() {
-  ./configure --prefix $pkg_prefix -no-graph && make world
+  ./configure --prefix $pkg_prefix -no-graph && make world.opt
 }
 
 do_check() {
